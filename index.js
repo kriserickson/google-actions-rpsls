bf24c86c1e99c055e8bc8a5503108fa96b60f34d
Kris Erickson <ksoncan34@yahoo.ca>
2018-02-04 09:21:50 -0800

    Initial script.

---
+++ index.js
@@ -0,0 +1,37 @@
+const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const expressApp = express();
+
+expressApp.use(bodyParser.urlencoded({extended: false}));
+expressApp.use(bodyParser.json({extended: false}));
+
+function mainIntentHandler(app) {
+    app.ask('Hey Mom, It works!');
+}
+
+expressApp.use((request, response) => {
+    console.log('Request body: ' + JSON.stringify(request.body));
+
+    const actionApp = new ActionsSdkApp({
+        request,
+        response
+    });
+
+    // Map that contains the intents and respective handlers to be used by the
+    // actions client library
+    const actionMap = new Map();
+
+    actionMap.set(actionApp.StandardIntents.MAIN, mainIntentHandler);
+
+    actionApp.handleRequestAsync(actionMap).then(() => {
+        console.log('Success handling GoogleAction request');
+    }, (reason) => {
+        console.error('Error: ' + JSON.stringify(reason));
+    });
+});
+
+
+expressApp.listen(5050, () => console.log('Example app listening on port ' + 5050));
+
